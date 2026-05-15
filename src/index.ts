e7e0d3f674379d9d9336400f934da8824ef2c19c
fbernstein <fcb613@gmail.com>
2026-05-15 13:28:58 -0400

    updated tsconfig w root dir

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from "./DataUtil.js"
-export * from "./UserStore.js"
-export * from "./useSessionTimeout.js"
+export * from "./DataUtil"
+export * from "./UserStore"
+export * from "./useSessionTimeout"
